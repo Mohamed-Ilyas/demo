ca206646acce3684b085dbbc37a7b7f887f664db
Mohamed Ilyas Mohamed Iqbal <mohamed.ilyas.mohamed.iqbal@ing.com>
2026-04-20 23:13:25 +0530

    second commit

--- src/Member.java
+++ src/Member.java
@@ -1,2 +1,18 @@
 public class Member extends User {
+    int borrowedBooksCount;
+    final int MAX_BORROW_LIMIT = 5;
+
+    // Step 4.4: Constructor
+    public Member(int borrowedBooksCount) {
+        this.borrowedBooksCount = borrowedBooksCount;
+    }
+
+    public void displayDashboard() {
+        System.out.println("Member Dashboard");
+        System.out.println("Books Borrowed: " + borrowedBooksCount);
+    }
+
+    public boolean canBorrowBooks() {
+        return borrowedBooksCount < MAX_BORROW_LIMIT;
+    }
 }
